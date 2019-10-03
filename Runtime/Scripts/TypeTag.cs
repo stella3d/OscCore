6d0c5b97ffa913eec36e0ff7e9a77463c8b37c2a
Stella Cannefax <stella@unity3d.com>
2019-10-03 01:28:49 -0700

    add extended type tags to enum

--- Runtime/Scripts/TypeTag.cs
+++ Runtime/Scripts/TypeTag.cs
@@ -1,11 +1,24 @@
 ﻿namespace OscCore
 {
+    // type tags from http://opensoundcontrol.org/spec-1_0 
     public enum TypeTag : byte
     {
-        Blob = 98,                      // b
-        Float32 = 102,                  // f
-        Int32 = 105,                    // i
-        String = 115                    // s
+        False = 70,                     // F, non-standard
+        Infinitum = 78,                 // I, non-standard
+        Nil = 78,                       // N, non-standard
+        AltTypeString = 84,             // S, non-standard
+        True = 84,                      // T, non-standard
+        ArrayStart = 91,                // [, non-standard
+        ArrayEnd = 93,                  // ], non-standard
+        Blob = 98,                      // b, STANDARD
+        AsciiChar32 = 99,               // c, non-standard
+        Double = 100,                   // d, non-standard
+        Float32 = 102,                  // f, STANDARD
+        Long = 104,                     // h, non-standard
+        Int32 = 105,                    // i, STANDARD
+        MIDI = 109,                     // m, non-standard
+        Color32 = 114,                  // r, non-standard
+        String = 115                    // s, STANDARD
     }
 }
 
