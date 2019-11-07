822e93d0bbf7824a0eee12d1f60a866c29e30d45
Stella Cannefax <stella@unity3d.com>
2019-11-06 21:29:27 -0800

    misc cleanup

--- Runtime/Scripts/Component/OscReceiver.cs
+++ Runtime/Scripts/Component/OscReceiver.cs
@@ -1,15 +1,17 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace OscCore
 {
+    /// <summary>Wraps an OscServer in a Unity Component</summary>
     [ExecuteAlways]
     public class OscReceiver : MonoBehaviour
     {
+        [Tooltip("The port to listen for incoming OSC messages on")]
         [SerializeField] int m_Port = 9000;
 
         OscServer m_Server;
 
+        /// <summary>The underlying OscServer</summary>
         public OscServer Server => m_Server;   
      
         void OnEnable()
