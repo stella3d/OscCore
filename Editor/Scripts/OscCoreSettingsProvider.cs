e4768546b952b437f8625df6dffd25d68af6eb12
Stella Cannefax <stella@unity3d.com>
2020-02-09 03:36:44 -0800

    add settings provider for help

---
+++ Editor/Scripts/OscCoreSettingsProvider.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace OscCore
+{
+    static class OscCoreSettingsIMGUIRegister
+    {
+        const string k_HelpPrefKey = "OscCore_ShowEditorHelp";
+        
+        const string k_HelpTooltip = "If enabled, display tutorial & hint messages in the Editor";
+        static readonly GUIContent k_HelpContent = new GUIContent("Show Help", k_HelpTooltip);
+        
+        [SettingsProvider]
+        public static SettingsProvider CreateOscCoreSettingsProvider()
+        {
+            // First parameter is the path in the Settings window.
+            // Second parameter is the scope of this setting: it only appears in the Project Settings window.
+            var provider = new SettingsProvider("User/Open Sound Control Core", SettingsScope.User)
+            {
+                // By default the last token of the path is used as display name if no label is provided.
+                label = "OSC Core",
+                // Create the SettingsProvider and initialize its drawing (IMGUI) function in place:
+                guiHandler = (searchContext) =>
+                {
+                    if (EditorPrefs.HasKey(k_HelpPrefKey))
+                    {
+                        var setting = EditorPrefs.GetBool(k_HelpPrefKey);
+                        var afterSetting = EditorGUILayout.Toggle(k_HelpContent, setting);
+                        if(afterSetting != setting)
+                            EditorPrefs.SetBool(k_HelpPrefKey, afterSetting);
+                    }
+                    else
+                    {
+                        EditorPrefs.SetBool(k_HelpPrefKey, true);
+                    }
+                },
+
+                // Populate the search keywords to enable smart search filtering and label highlighting:
+                keywords = new HashSet<string>(new[] { "OSC", "Help" , "Open Sound" })
+            };
+
+            return provider;
+        }
+    }
+}
+
+
