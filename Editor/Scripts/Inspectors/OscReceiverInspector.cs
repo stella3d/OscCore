69f70f9ac81f1bd704bc6bbee1369e5e9a7eb2cc
Stella Cannefax <stella@unity3d.com>
2020-02-09 02:42:35 -0800

    add osc receiver custom inspector and editor scripts folder

---
+++ Editor/Scripts/Inspectors/OscReceiverInspector.cs
@@ -0,0 +1,22 @@
+﻿using UnityEditor;
+
+namespace OscCore
+{
+    [CustomEditor(typeof(OscReceiver))]
+    public class OscReceiverInspector : Editor
+    {
+        SerializedProperty m_PortProp;
+       
+        void OnEnable()
+        {
+            m_PortProp = serializedObject.FindProperty("m_Port");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.PropertyField(m_PortProp);
+        }
+    }
+}
+
+
