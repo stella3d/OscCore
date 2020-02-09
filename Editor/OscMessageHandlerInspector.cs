3a86477e2fd84356ac42262b10cda9823775d9db
Stella Cannefax <stella@unity3d.com>
2020-02-09 02:38:46 -0800

    add custom inspector for message handler components

---
+++ Editor/OscMessageHandlerInspector.cs
@@ -0,0 +1,30 @@
+﻿using UnityEditor;
+
+namespace OscCore
+{
+    [CustomEditor(typeof(OscMessageHandler<,>), true)]
+    public class OscMessageHandlerInspector : Editor
+    {
+        SerializedProperty m_ReceiverProp;
+        SerializedProperty m_AddressProp;
+        SerializedProperty m_HandlerProp;
+       
+        void OnEnable()
+        {
+            m_ReceiverProp = serializedObject.FindProperty("m_Receiver");
+            m_AddressProp = serializedObject.FindProperty("m_Address");
+            m_HandlerProp = serializedObject.FindProperty("Handler");
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUILayout.PropertyField(m_ReceiverProp);
+            EditorGUILayout.PropertyField(m_AddressProp);
+            EditorGUILayout.Space();
+            if (m_HandlerProp != null)
+                EditorGUILayout.PropertyField(m_HandlerProp);
+        }
+    }
+}
+
+
