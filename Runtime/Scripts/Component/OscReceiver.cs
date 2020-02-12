2fb8cd44302c95e5c716be82e30ce02e3dd1bd37
Stella Cannefax <stella@unity3d.com>
2020-02-12 01:53:09 -0800

    support setting new port in OscReceiver

--- Runtime/Scripts/Component/OscReceiver.cs
+++ Runtime/Scripts/Component/OscReceiver.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace OscCore
 {
@@ -10,8 +11,13 @@ namespace OscCore
         [Tooltip("The local port to listen for incoming messages on")]
         [SerializeField] int m_Port = 9000;
 
-        /// <summary>The local post to listen to incoming messages on</summary>
-        public int Port => m_Port;
+        // TODO - add setter here and support port switching
+        /// <summary>The local port to listen to incoming messages on</summary>
+        public int Port
+        {
+            get => m_Port;
+            set => SetPort(value);
+        }
 
         /// <summary>True if this receiver is bound to its port and listening, false otherwise</summary>
         public bool Running { get; private set; }
@@ -45,6 +51,27 @@ namespace OscCore
         void OnApplicationQuit()
         {
             Server?.Dispose();
+        }
+
+        void SetPort(int newPort)
+        {
+            var clamped = newPort.ClampPort();
+            if (clamped != newPort) return;
+
+            var oldValue = m_Port;
+            var oldServer = Server;
+            try
+            {
+                Server = OscServer.GetOrCreate(newPort);
+                m_Port = newPort;
+                oldServer?.Dispose();
+            }
+            // if creating a new server throws for any reason, make sure to keep old settings
+            catch (Exception e)
+            {
+                m_Port = oldValue;
+                Server = oldServer;
+            }
         }
     }
 }
