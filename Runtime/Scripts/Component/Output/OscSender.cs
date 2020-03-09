90543a4fd99e691fd72f6403b650eb82986e4fb4
Anthony Burchell <antpb@Anthonys-MacBook-Pro.local>
2020-03-09 17:10:34 -0500

    adds reinit function

--- Runtime/Scripts/Component/Output/OscSender.cs
+++ Runtime/Scripts/Component/Output/OscSender.cs
@@ -43,6 +43,12 @@ namespace OscCore
             if(Client == null)
                 Client = new OscClient(m_IpAddress, m_Port);
         }
+
+        public void ReInit()
+        {
+            Client = null;
+            Setup();
+        }
     }
 }
 
