574d00d551b0bfb5ccfdc8e8b1fca5e05ed8572c
Stella Cannefax <stella.cannefax+git@gmail.com>
2021-03-18 10:30:13 -0700

    re-org of sequence related test files

---
+++ Tests/Runtime/Sequences/MessageSequenceSender.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace OscCore.Tests
+{
+    public class MessageSequenceSender : MonoBehaviour
+    {
+        public OscMessageSequence Sequence;
+
+        public OscSender Sender;
+
+        int m_LastSentIndex = 0;
+        TimedMessage m_NextMessage;
+
+        public void Start()
+        {
+            if(Sequence == null) 
+            {
+                this.enabled = false;
+                return;
+            }
+
+            m_NextMessage = Sequence.Messages[0];
+        }
+
+        public void Update()
+        {
+            var t = Time.time;
+            if(t >= m_NextMessage.Time)
+            {
+                var msg = m_NextMessage.Message;
+                Sender.Client.Send(msg.Address, msg.TypeTags, msg.Data);
+            }
+        }
+    }
+}
